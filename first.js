affcbe6ad032d80253d86ab7a90a0c04284a0964
avi-verma-web <aakash.t.yadav@slrtce.in>
2025-10-31 09:16:54 +0530

    my first git commot

---
+++ first.js
@@ -0,0 +1,17 @@
+// console.log('This is the first JavaScript file.');  
+console.log('This is the first line of my JavaScript file.');
+
+// let c = 90
+// c = 100
+// console.log(c)
+// var a = 10;
+// var b = 122;
+// console.log(a+b);
+
+
+// let a = 10
+// let b = 20
+// let c = 30 
+// console.log(a*b+c)
+
+
