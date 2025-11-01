67b0676ee87fe1fd2232e3d11d8828e2c0989d6c
avi-verma-web <aakash.t.yadav@slrtce.in>
2025-11-01 20:44:22 +0530

    my second file

---
+++ second.js
@@ -0,0 +1,66 @@
+// datatype conversion
+
+// let score = "33"
+// console.log(typeof(score))
+
+// let value = Number(score)
+// console.log(typeof value)
+
+
+// let so = "33abc"
+// console.log(typeof(score))
+
+// let np = Number(so)
+// console.log(typeof value)
+// console.log(np) //here it will print NaN because it not a pure number 
+
+
+// let isloggedin = 0
+// let BooleanIsLoggedIn = Boolean(isloggedin)
+// console.log(BooleanIsLoggedIn)
+//1 =>ture , 0=>false
+//''=> false ,'aakash'=> true
+
+
+//****************************operator *****************************
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// console.log("2"+2)
+// console.log(2+"2") 
+// console.log("1"+2+2)   //here if string is used as a first operand than it will treat everthing as a string
+// console.log(1+2+"2")
+
+
+// let count = 100
+// count++
+// console.log(count)
+
+
+//*******************************comparison ***************************
+
+// console.log("2">1);
+// console.log("2">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+// const hero = ["shaktiman", "naagraj","doga"]
+// let myobj = {
+//     name:"hitesh",
+//     age:22,
+// }
+
+// console.log(myobj)
+
+
+// const myfunction = function(){
+//     console.log("hello world");
+// }
+// console.log(typeof myfunction)
